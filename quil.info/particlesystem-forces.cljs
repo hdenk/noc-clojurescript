0d4e7b9805bc0242aa91d57df7c2eca055994c51
nuryokusan <nuryokusan@gmail.com>
2018-07-10 08:43:34 +0200

    refactored particlesystem-forces do not use protocols

--- quil.info/particlesystem-forces.cljs
+++ quil.info/particlesystem-forces.cljs
@@ -24,123 +24,150 @@
 
 (ns nature-of-code.systems.particlesystem-forces.core
   "Particle-System produces Particles that experience Gravity
-	 Based on the Nature of Code by Daniel Shiffman http://natureofcode.com"
+  	 Based on the Nature of Code by Daniel Shiffman http://natureofcode.com"
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
             [nature-of-code.math.vector :as mv]))
 
 (def params 
-  {:size [600 400]
+  {:size [600 600]
    :background 255
    :frame-rate 30
-   :gravity [0.0 0.1]
+   :gravity [0.0 0.05]
    :lifespan 255
    :lifespan-dec-rate 2
-   :particle-r 16
-   :particle-color 127}) 
+   :circle-r 16
+   :square-l 16
+   :particle-color 127
+   :particle-outline-thickness 2}) 
 
-(defn size-x []
+(defmacro SIZE-X []
   (first (params :size)))
 
-(defn size-y []
+(defmacro SIZE-Y []
   (second (params :size)))
 
 ;;
-;; Abstractions
+;; particle
 ;;
 
-(defprotocol Movable_ ; hmm... calling it Movable results in failure
-  (move [this] "enter next motion state for the mobile object"))
-
-(defprotocol Massiv
-  (apply-force [this force] "apply force to the massive object"))
-
-(defprotocol Expirable
-  (expired? [this] "returns true when lifespan is over"))
-
-(defprotocol Drawable
-  (draw [this] "draw the drawable object to an output-device"))
-
-;;
-;; Particle
-;;
-
-(defrecord Particle [id mass location velocity acceleration lifespan]
-  Movable_  
-  (move [this]
-    (let [next-location (mv/add location velocity)
-          next-velocity (mv/add velocity acceleration)
-          next-acceleration (mv/multiply acceleration 0)
-          next-lifespan (- lifespan (params :lifespan-dec-rate))]
-      (assoc this :location next-location :velocity next-velocity :acceleration next-acceleration :lifespan next-lifespan)))
-
-  Massiv
-  (apply-force [this force] 
-    (let [mf (mv/divide force (float mass))
-          next-acceleration (mv/add acceleration mf)]
-      (assoc this :acceleration next-acceleration)))
-
-  Expirable
-  (expired? [this] 
-    (< lifespan 0))
-
-  Drawable
-  (draw [this]
-    (q/stroke 0 lifespan)
-    (q/stroke-weight 2)
-    (q/fill (params :particle-color) lifespan)
-    (q/ellipse (first location) (second location) (params :particle-r) (params :particle-r))
-    this))
-
+;; TODO clojure.spec verwenden ?)
 (defn gen-particle 
-  [& {:keys [id mass location velocity acceleration lifespan] 
-      :or {id "px" mass 0 location [0 0] velocity [0 0] acceleration [0 0] lifespan 0}}] 
-  (Particle. id mass location velocity acceleration lifespan)) 
+  [& {:keys [id shape mass location velocity acceleration lifespan] 
+      :or {}}] 
+  {:id id :shape shape :mass mass :location location :velocity velocity :acceleration acceleration :lifespan lifespan}) 
+
+(defn apply-force-to-particle [{:keys [mass acceleration] :as particle} force] 
+  (let [mf (mv/divide force (float mass))
+        next-acceleration (mv/add acceleration mf)]
+    (assoc particle :acceleration next-acceleration)))
+
+(defn move-particle [{:keys [mass location velocity acceleration lifespan] :as particle}]
+  (let [next-location (mv/add location velocity)
+        next-velocity (mv/add velocity acceleration)
+        next-acceleration (mv/multiply acceleration 0)
+        next-lifespan (- lifespan (params :lifespan-dec-rate))]
+    (assoc particle :location next-location :velocity next-velocity :acceleration next-acceleration :lifespan next-lifespan)))
+
+(defn next-particle [particle force]
+  (-> particle 
+      (apply-force-to-particle force)
+      (move-particle)))
+
+(defn is-expired-particle? [{:keys [lifespan] :as particle}] 
+  (< lifespan 0))
+
+(defmulti draw-particle (fn [particle] (:shape particle)))
+
+(defmethod draw-particle :circle [{:keys [location lifespan] :as particle}]
+  (q/stroke 0 lifespan)
+  (q/stroke-weight (params :particle-outline-thickness))
+  (q/fill (params :particle-color) lifespan)
+  (q/ellipse (first location) (second location) (params :circle-r) (params :circle-r))
+  particle)
+
+(defmethod draw-particle :square [{:keys [location lifespan] :as particle}]
+  (q/stroke 0 lifespan)
+  (q/stroke-weight (params :particle-outline-thickness))
+  (q/fill (params :particle-color) lifespan)
+  (q/rect-mode :center)
+  (q/rect (first location) (second location) (params :square-l) (params :square-l)) 
+  (q/rect-mode :corner) ; TODO? get current rect-mode from graphic object
+  particle)
+
+(defmethod draw-particle :rotating-square [{:keys [location lifespan] :as particle}]
+  (q/stroke 0 lifespan)
+  (q/stroke-weight (params :particle-outline-thickness))
+  (q/fill (params :particle-color) lifespan)
+  (q/push-matrix)
+  (q/translate (first location) (second location))
+  (let [theta (q/map-range (first location) 0 (q/width) 0 (* Math/PI 2))]
+    (q/rotate theta))
+  (q/rect-mode :center)
+  (q/rect 0 0 (params :square-l) (params :square-l)) 
+  (q/rect-mode :corner) ; TODO? get current rect-mode from graphic object
+  (q/pop-matrix)
+  particle)
 
 ;;
-;; ParticleSystem
+;; particle-system
 ;;
 
-(defn move-particles [particles]
-  (map move particles))
+;; TODO clojure.spec verwenden ?)
+(defn gen-particle-system
+  [& {:keys [origin gravity particles] 
+      :or {}}]
+  {:origin origin :gravity gravity :particles particles})
 
-(defn add-particle [particles origin]
-  (conj 
-    particles 
-    (gen-particle :id (str "p" (count particles)) :mass 1.0 :location origin :velocity [(q/random -1.0 1.0) (q/random -2.0 0)] :lifespan (params :lifespan))))
+(defn next-particles [{:keys [gravity particles] :as particle-system}]
+  (let [next-particles (into [] (map (fn [particle] (next-particle particle gravity))
+                                     particles))]
+    (assoc particle-system :particles next-particles)))
 
-(defn remove-expired [particles]
-  (remove expired? particles)) 
+(defn random-shape []
+  (rand-nth [:circle :square :rotating-square]))
 
-(defrecord ParticleSystem [origin particles]
-  Movable_
-  (move [this]
-    (let [ next-particles 
-          (-> particles 
-            (move-particles) 
-            (add-particle origin) 
-            (remove-expired))]
-      (assoc this :particles next-particles))) 
+(defn add-new-particle [{:keys [origin particles] :as particle-system}]
+  (let [next-particles (conj particles 
+                         (gen-particle 
+                           :id (str "p" (count particles)) 
+                           :shape (random-shape)
+                           :mass 1.0 
+                           :location origin 
+                           :velocity [(q/random -1.0 1.0) (q/random -2.0 0.0)] 
+                           :acceleration [0 0]
+                           :lifespan (params :lifespan)))]
+    (assoc particle-system :particles next-particles)))
 
-  Massiv
-  (apply-force [this force]
-    (let [next-particles (map #(apply-force % force) particles)]
-      (assoc this :particles next-particles))) 
+(defn remove-expired-particles [{:keys [particles] :as particle-system}]
+  (let [next-particles (remove is-expired-particle? particles)]
+    (assoc particle-system :particles next-particles)))  
 
-  Drawable
-  (draw [this]
-    (dorun (map #(draw %) particles))
-     this))
+(defn next-particle-system [particle-system]
+  (-> particle-system 
+      (next-particles) 
+      (add-new-particle) 
+      (remove-expired-particles)))
+
+(defn draw-particle-system [{:keys [particles] :as particle-system}]
+  (dorun (map (fn [particle] (draw-particle particle)) particles))
+  particle-system)
 
 ;;
 ;; Sketch
-;;
+;; 
 
-(def particle-system (atom (map->ParticleSystem {:origin [(/ (size-x) 2) (- (size-y) (* (size-y) 0.75))] :particles []})))
+(def particle-system (atom (gen-particle-system 
+                             :origin [(/ (SIZE-X) 2) (- (SIZE-Y) (* (SIZE-Y) 0.75))] 
+                             :gravity (params :gravity)
+                             :particles [])))
 
 (defn setup-sketch []
+  ;(js/console.log "setup-sketch")
   (q/frame-rate (params :frame-rate))
-  (q/smooth))
+  (q/smooth)
+  (q/rect-mode :corner))
+
 
 (defn draw-sketch []
   ; draw Background
@@ -148,16 +175,11 @@
   (q/fill 255) 
   (q/rect 0 0 (q/width) (q/height))
 
-  ; draw Particles
-  (draw @particle-system)
+  ; draw particle-system
+  (draw-particle-system @particle-system)
 
   ; update ParticleSystem to next-state
-  (let [gravity (params :gravity)]
-    (swap! 
-      particle-system 
-      #(-> % 
-         (apply-force gravity) 
-         (move)))))
+  (swap! particle-system next-particle-system)) 
 
 ; This sketch uses functional-mode middleware.
 ; Check quil wiki for more info about middlewares and particularly
